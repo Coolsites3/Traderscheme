4e2b799406f6774f1ef449b9c825625f1470a544
Coolsites3 <brandkingjay@gmail.com>
2026-09-10 10:12:51 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   env: {
     NEXT_PUBLIC_DERIV_MARKUP_PERCENT: process.env.NEXT_PUBLIC_DERIV_MARKUP_PERCENT || '3',
   },
